a2c0558e3cc66617df8de0b685a25a5a5b9d068b
Estefania <estefacdn@gmail.com>
2021-12-02 16:33:48 -0500

    Agregar archivos de proyecto.

---
+++ Controllers/MenuController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Restaurants.Models;
+using Restaurants.Models.ViewModels;
+
+namespace Restaurants.Controllers
+{
+    public class MenuController : Controller
+    {
+        // GET: Menu
+      
+        public ActionResult Menu()
+        {
+            ViewBag.mostrar = "Menu";
+            return View();
+        }
+    }
+}
---
+++ Models/ViewModels/ListTablaViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Restaurants.Models.ViewModels
+{
+    public class ListTablaViewModel
+    {
+        public int idplato {get;set;}
+        public String nombreplato {get;set;}
+        public int numeromesa {get;set;}
+        public int valor {get;set;}
+        public String nombremesero {get;set;}
+
+    }
+}
